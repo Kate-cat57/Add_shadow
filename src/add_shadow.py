63e0fcb731189e63df320f757ebd65a15507e191
Kate-cat57 <69852665+Kate-cat57@users.noreply.github.com>
2021-03-29 12:44:09 +0300

    Update add_shadow.py

--- src/add_shadow.py
+++ src/add_shadow.py
@@ -10,7 +10,8 @@ TEAM_ID = int(os.environ['context.teamId'])
 WORKSPACE_ID = int(os.environ['context.workspaceId'])
 PROJECT_ID = int(os.environ['modal.state.slyProjectId'])
 RESULT_PROJECT_NAME = os.environ["modal.state.projectName"]
-TRANSPARENCE = float(os.environ["modal.state.transparence"])
+MIN_TRANSPARENCE = float(os.environ["modal.state.mintransparence"])
+MAX_TRANSPARENCE = float(os.environ["modal.state.maxtransparence"])
 POBABILITY = float(os.environ["modal.state.probability"])
 MAX_SHADOWS = int(os.environ["modal.state.maxShadow"])
 
@@ -95,7 +96,8 @@ def add_shadow(api: sly.Api, task_id, context, state, app_logger):
             # invert and convert shadow matrix
             invert_shadow = np.invert(shadow)
             convert_shadow = invert_shadow.astype(float)
-
+            
+            
             convert_shadow[convert_shadow == 0] = TRANSPARENCE  # add opacity
 
             img[:, :, 0] = img[:, :, 0] * convert_shadow
